894e84a52226dfda12e5715a763d95030d6e3280
tomas ruiz <truiz050904@gmail.com>
2023-03-01 20:15:43 -0300

    ejercicio 4.4: Se agrego bootstrap al proyecto.

---
+++ src/Controller/mainController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class mainController extends AbstractController
+{
+    /**
+     * @Route("/tienda2023")
+     */
+    public function saludo_tienda_2023():Response
+    {
+         
+        // get the user information and notifications somehow
+        $userFirstName = '...';
+        $userNotifications = ['...', '...'];
+
+        // the template path is the relative file path from `templates/`
+        return $this->render('base.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'user_first_name' => $userFirstName,
+            'notifications' => $userNotifications,
+        ]);
+    
+    }
+
+    /**
+     * @Route("/")
+     */
+    public function main(){
+    return new Response ("hola");
+    }
+   
+}
