72e137b7f88d3d8724a16222739e12b6e32d2794
tomas ruiz <truiz050904@gmail.com>
2023-03-03 19:15:06 -0300

    En este archivo encontramos los controladores necesarios para que nuestra app funcione correctamente, acan controlamos las solicitudes de api, las rutas, etc. Esta hecho en php usando el framework Symfony.

--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -6,6 +6,7 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class mainController extends AbstractController
 {
@@ -28,24 +29,58 @@ class mainController extends AbstractController
     /**
      * @Route("/productos", name="productos")
      */
-    public function productos(HttpClientInterface $client): Response
+    public function productos(HttpClientInterface $product, Request $request): Response
     {
-        $content = $this->fetchFakeApiInformation($client);
+        $products = $this->fetchFakeApiInformation($product, $request);
 
         return $this->render('productos.html.twig', [
-            'content' => $content,
+            'products' => $products,
         ]);
     }
 
-    public function fetchFakeApiInformation(HttpClientInterface $client): array
+    public function fetchFakeApiInformation(HttpClientInterface $product, Request $request): array
     {
-        $response = $client->request(
-            'GET',
-            'https://fakerapi.it/api/v1/products?_quantity=10&_taxes=12&_categories_type=uuid'
-        );
+        $quantity = $request->query->getInt('quantity', 30);
+        $minPrice = $request->query->getInt('min_price', 0);
+        $maxPrice = $request->query->getInt('max_price', 999999);
+        $taxes = $request->query->getInt('taxes', 12);
 
-        $content = $response->toArray();
+        $url = sprintf('https://fakerapi.it/api/v1/products?_quantity=%d&_taxes=%d&_categories_type=uuid&_price_min=%d&_price_max=%d', $quantity, $taxes, $minPrice, $maxPrice);
 
-        return $content;
+        $response = $product->request('GET', $url);
+
+        $products = $response->toArray();
+
+        return $products;
     }
+
+ 
+/**
+ * @Route("/clientes", name="clientes")
+ */
+public function clientes(HttpClientInterface $httpClient, Request $request): Response
+{
+    $clientes = $this->fetchFakeApiInformationClientes($httpClient, $request);
+
+    return $this->render('clientes.html.twig', [
+        'clientes' => $clientes,
+    ]);
+}
+
+public function fetchFakeApiInformationClientes(HttpClientInterface $httpClient, Request $request): array
+{
+    $quantity = $request->query->getInt('quantity');
+    $gender = $request->query->get('gender', 'all');
+    $minBirthdate = $request->query->get('min_birthdate', '1950-01-01');
+    $maxBirthdate = $request->query->get('max_birthdate', '2000-12-31');
+
+    $url = sprintf('https://fakerapi.it/api/v1/persons?_quantity=%d&gender=%s&birthdate_min=%s&birthdate_max=%s', $quantity, $gender, $minBirthdate, $maxBirthdate);
+
+    $response = $httpClient->request('GET', $url);
+
+    $clientes = $response->toArray();
+
+    return $clientes;
+}
+
 }
